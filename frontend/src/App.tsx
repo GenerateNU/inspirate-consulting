0a8ad1e12d89c47398033b3a62369cb4ad86c325
Josh Torre <torre.jo@northeastern.edu>
2026-09-09 17:05:43 -0400

    Init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <p className="text-6xl font-semibold w-auto h-auto">
+        Howdy Inspirate Consulting Engineers! 👋🤠
+      </p>
+    </div>
+  );
+}
+
+export default App;
