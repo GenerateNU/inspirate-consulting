38c710fc637a7afe4f57f61dc28a87dfede0a376
Shikha Amin <shikha.amin07@gmail.com>
2026-09-20 17:08:02 -0400

    Add TodoItem model

---
+++ backend/internal/models/todo-item.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type TodoItem struct {
+	ID              string     `json:"id"`
+	CreatedAt       time.Time  `json:"created_at"`
+	UpdatedAt       time.Time  `json:"updated_at"`
+	StudentID       string     `json:"student_id"`
+	UserID          string     `json:"user_id"`
+	TodoDescription string     `json:"todo_description"`
+	CompletedAt     *time.Time `json:"completed_at"`
+	Deadline        *time.Time `json:"deadline"`
+}
