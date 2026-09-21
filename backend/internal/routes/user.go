2c047991937f0cb924192acf366d3f11434cfca8
Alen <alenganopolsky@gmail.com>
2026-09-20 23:10:44 -0400

    added models for create/get user input/outpu

--- backend/internal/routes/user.go
+++ backend/internal/routes/user.go
@@ -5,28 +5,25 @@ import (
 	"net/http"
 
 	"inspirate-consulting/internal/data"
-	"inspirate-consulting/internal/handlers/greeting"
+	"inspirate-consulting/internal/handlers/user"
 	"inspirate-consulting/internal/models"
 
 	"github.com/danielgtaylor/huma/v2"
 )
 
-func CreateUser(api huma.API, repository *data.Repository) {
-	greetingHandler := greeting.NewHandler(repository.Greeting)
-	// Register POST /greeting handler.
-	// The handler function takes in a struct that defines its inputs ('Body' in this case)
-	// and returns the CreateGreetingOutput model built in the models
+func SetupUserRoutes(api huma.API, repository *data.Repository) {
+	userHandler := user.NewHandler(repository.User)
 	huma.Register(api, huma.Operation{
-		OperationID: "create-greeting",
+		OperationID: "create-user",
 		Method:      http.MethodPost,
-		Path:        "/greeting",
-		Description: "Create a greeting for a person by name.",
-		Tags:        []string{"Greetings"},
-	}, func(ctx context.Context, input *models.CreateGreetingInput) (*models.CreateGreetingOutput, error) {
-		greeting, err := greetingHandler.CreateGreeting(ctx, input)
+		Path:        "/user",
+		Description: "Create a user (student/counselor)",
+		Tags:        []string{"User"},
+	}, func(ctx context.Context, input *models.CreateUserInput) (*models.CreateUserOutput, error) {
+		userOutput, err := userHandler.CreateUser(ctx, input)
 		if err != nil {
 			return nil, err
 		}
-		return greeting, nil
+		return userOutput, nil
 	})
 }
