6c3c972a17b8e864c6990fba34fb670bd8ebef95
Meredith Wanta <wanta.m@northeastern.edu>
2026-09-21 11:36:52 -0400

    personal college application backend

--- backend/internal/handlers/personal_college_application/handler.go
+++ backend/internal/handlers/personal_college_application/handler.go
@@ -4,11 +4,13 @@ import storage "inspirate-consulting/internal/data"
 
 type Handler struct {
 	PersonalCollegeApplicationRepository storage.PersonalCollegeApplicationRepository
+	GlobalCollegeRepository storage.GlobalCollegeRepository
 }
 
 // Here is where we create the handler which has reference to the PersonalCollegeApplicationRepository
-func NewHandler(personalCollegeApplicationRepository storage.PersonalCollegeApplicationRepository) *Handler {
+func NewHandler(personalCollegeApplicationRepository storage.PersonalCollegeApplicationRepository, globalCollegeRepository storage.GlobalCollegeRepository) *Handler {
 	return &Handler{
 		PersonalCollegeApplicationRepository: personalCollegeApplicationRepository,
+		GlobalCollegeRepository: globalCollegeRepository,
 	}
 }
