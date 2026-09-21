9216e9b1a46ab11defaecb31588436a429182990
Alen <alenganopolsky@gmail.com>
2026-09-20 23:10:44 -0400

    added models for create/get user input/outpu

--- backend/internal/models/users.go
+++ backend/internal/models/users.go
@@ -37,3 +37,22 @@ type Counselor struct {
 	ID     uuid.UUID `json:"id"`
 	UserID uuid.UUID `json:"user_id"`
 }
+
+type CreateUserInput struct {
+	Body struct {
+		Name   string  `json:"name" db:"name" doc:"Name of the user" minLength:"1" maxLength:"200"`
+		PfpKey *string `json:"pfp_key" db:"pfp_key" doc:"pfp key of user"`
+	}
+}
+
+type CreateUserOutput struct {
+	Body *User `json:"body"`
+}
+
+type FetchUserInput struct {
+	ID uuid.UUID `path:"id" required:"true"`
+}
+
+type FetchUserOutput struct {
+	Body *User `json:"body"`
+}
