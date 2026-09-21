9b674545f58fb6e4250c2459cf6a95209dd15474
Alen <alenganopolsky@gmail.com>
2026-09-20 20:56:58 -0400

    debugging & slight modifications

--- backend/internal/supabase/migrations/20260921000213_create_user_tables.sql
+++ backend/internal/supabase/migrations/20260921000213_create_user_tables.sql
@@ -1,23 +1,29 @@
-
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TYPE YEAR AS ENUM ('freshman', 'sophomore', 'junior', 'senior');
 
-CREATE TABLE IF NOT EXISTS user {
+CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
-    supabase_id UUID gen_random_uuid(),
-    pfp_key TEXT
-};
+    supabase_id UUID DEFAULT gen_random_uuid(),
+    pfp_key TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
 
-CREATE TABLE IF NOT EXISTS student {
+CREATE TABLE IF NOT EXISTS counselor (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES user(id),
+    user_id UUID NOT NULL REFERENCES users(id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS student (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id),
     year YEAR NOT NULL,
     organization TEXT,
     review_balance INT NOT NULL,
-    counselor_id UUID NOT NULL REFERENCES counselor(id)
-};
-
-CREATE TABLE IF NOT EXISTS counselor {
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES user(id)
-};
+    counselor_id UUID NOT NULL REFERENCES counselor(id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
