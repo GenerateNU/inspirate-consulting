5c68acf647d387f05f6268c3abfd801760d7720a
Shikha Amin <shikha.amin07@gmail.com>
2026-09-21 13:25:39 -0400

    formatting all 4 files

--- backend/internal/handlers/todo_item/handler.go
+++ backend/internal/handlers/todo_item/handler.go
@@ -4,12 +4,12 @@ import (
 	storage "inspirate-consulting/internal/data"
 )
 
-type Handler struct{
+type Handler struct {
 	TodoItemRepository storage.TodoItemRepository
 }
 
-//Handler with reference to TodoItemRepository
-func NewHandler(todoItemRepository storage.TodoItemRepository)(*Handler){
+// Handler with reference to TodoItemRepository
+func NewHandler(todoItemRepository storage.TodoItemRepository) *Handler {
 	return &Handler{
 		TodoItemRepository: todoItemRepository,
 	}
