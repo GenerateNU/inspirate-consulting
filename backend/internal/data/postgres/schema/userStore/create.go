f8cf6cfb7030a25b93956c028ac9d6d135efc73e
Alen <alenganopolsky@gmail.com>
2026-09-22 00:45:17 -0400

    removed logging

--- backend/internal/data/postgres/schema/userStore/create.go
+++ backend/internal/data/postgres/schema/userStore/create.go
@@ -12,20 +12,13 @@ import (
 )
 
 func (r *UserRepository) CreateUser(ctx context.Context, user models.CreateUserInput, supabase_id uuid.UUID) (*models.CreateUserOutput, error) {
-<<<<<<< HEAD
-=======
-	fmt.Println("[db] CreateUser called")
->>>>>>> a6f19c6 (fixed create flow)
 	createdUser := &models.CreateUserOutput{Body: &models.User{}}
 
 	query, err := schema.ReadSQLBaseScript("create_user.sql", SqlUserFiles)
 	if err != nil {
-		fmt.Println("[db] ReadSQLBaseScript error:", err)
 		err := errs.InternalServerError("Failed to read base query: ", err.Error())
 		return nil, err
 	}
-
-	fmt.Println("[db] CreateUser gurt")
 
 	err = r.db.QueryRow(
 		ctx,
--- backend/internal/handlers/user/create.go
+++ backend/internal/handlers/user/create.go
@@ -2,26 +2,21 @@ package user
 
 import (
 	"context"
-	"fmt"
 	"inspirate-consulting/internal/auth"
 	"inspirate-consulting/internal/config"
 	"inspirate-consulting/internal/models"
 )
 
 func (h *Handler) CreateUser(ctx context.Context, input *models.CreateUserInput, config *config.Config) (*models.CreateUserOutput, error) {
-	fmt.Println("[handler] CreateUser called")
 
 	signup_response, err := auth.SupabaseSignup(&config.Supabase, input.Body.Email, input.Body.Password)
 	if err != nil {
-		fmt.Println("[handler] SupabaseSignup error:", err)
 		return nil, err
 	}
-	fmt.Println("[handler] SupabaseSignup success")
 
 	supabase_id := signup_response.User.ID
 	user, err := h.UserRepository.CreateUser(ctx, *input, supabase_id)
 	if err != nil {
-		fmt.Println("[handler] DB CreateUser error:", err)
 		return nil, err
 	}
 
