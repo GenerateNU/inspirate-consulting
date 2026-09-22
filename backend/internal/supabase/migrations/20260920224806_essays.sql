57d7388479396e1b53057a1fd491c540dbc3d1c1
Shawne Laquindanum <shawnebenedict@Outlook.com>
2026-09-22 00:42:16 -0400

    bug fixes

--- backend/internal/supabase/migrations/20260920224806_essays.sql
+++ backend/internal/supabase/migrations/20260920224806_essays.sql
@@ -2,7 +2,7 @@ create table essays (
     id uuid primary key default gen_random_uuid(),
     student_id uuid not null references students(id), 
     type text not null,
-    college_id uuid references global_college_info(id),
-    link_to_content text not null
+    college_id bigint references global_colleges(id),
+    link_to_content text not null,
     status text not null default 'Draft'
-)
+);
