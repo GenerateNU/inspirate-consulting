3af1dde90225dbf385a9c3c340d76a58536635b5
Shikha Amin <shikha.amin07@gmail.com>
2026-09-23 15:10:45 -0400

    Add TaskList page and update routes in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,7 @@ import CreateCollege from './college/create-college'
 import CollegeList from './college/college-list'
 
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
