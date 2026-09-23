176122967729d305fb968da5ed144b70bf26b378
Shikha Amin <shikha.amin07@gmail.com>
2026-09-23 00:43:08 -0400

    Added userID stub into placeholder auth

--- backend/internal/auth/placeholder-auth.go
+++ backend/internal/auth/placeholder-auth.go
@@ -5,3 +5,7 @@ import "context"
 func GetStudentID(ctx context.Context) string {
     return "00000000-0000-0000-0000-000000000002"
 }
+
+func GetUserID(ctx context.Context) string {
+    return "00000000-0000-0000-0000-000000000001"
+}
