362f8d0523317c5cabaa9c010fef50d4059b4973
Shawne Laquindanum <shawnebenedict@Outlook.com>
2026-09-22 22:20:28 -0400

    spotted a bug

--- backend/internal/models/essays.go
+++ backend/internal/models/essays.go
@@ -14,8 +14,8 @@ const (
 type Essays struct {
 	ID uuid.UUID `json.:"id"`
 	StudentID uuid.UUID `json:"student_id"`
-	type string `json:"type"`
-	CollegeID *uuid.UUID `json:"college_id"`
+	Type string `json:"type"`
+	CollegeID *int64 `json:"college_id"`
 	LinkToContent string `json:"link_to_content"`
 	/* I added this to help the studetns keep track of their progress*/
 	Status Status 
