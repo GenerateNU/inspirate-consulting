807ce9f36dde0b348d985e45218732484a3a6685
Shawne Laquindanum <shawnebenedict@Outlook.com>
2026-09-25 17:38:34 -0400

    typo lol

--- backend/internal/supabase/migrations/20260920224806_essays.sql
+++ backend/internal/supabase/migrations/20260920224806_essays.sql
@@ -3,7 +3,7 @@ CREATE TYPE status AS ENUM ('Submitted', 'Draft', 'Review', 'Archived');
 CREATE TABLE essays (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     /* this is a fk table does not exist yet though so its a place holder*/
-    student_id UUID NOT NULL DEFAULT gen_randome_uuid() /*REFERENCES students(id)*/,
+    student_id UUID NOT NULL DEFAULT gen_random_uuid() /*REFERENCES students(id)*/,
     type TEXT NOT NULL,
     college_id BIGINT REFERENCES global_colleges(id),
     link_to_content TEXT NOT NULL,
