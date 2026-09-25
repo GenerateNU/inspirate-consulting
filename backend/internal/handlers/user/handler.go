4ba32379cbaf7dfd3284a970e70d0c36290d7683
Alen <alenganopolsky@gmail.com>
2026-09-24 20:28:42 -0400

    removed comments

--- backend/internal/handlers/user/handler.go
+++ backend/internal/handlers/user/handler.go
@@ -4,14 +4,10 @@ import (
 	storage "inspirate-consulting/internal/data"
 )
 
-// The greeting Handler has a dependency on GreetingRepository
-// in other words, greeting handler logic depends on there being a
-// GreetingRepository we can call on to pass down for DB operations
 type Handler struct {
 	UserRepository storage.UserRepository
 }
 
-// Here is where we create the handler which has reference to the GreetingRepository
 func NewHandler(userRepository storage.UserRepository) *Handler {
 	return &Handler{
 		UserRepository: userRepository,
