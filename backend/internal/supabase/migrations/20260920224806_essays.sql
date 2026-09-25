af87063d8e5369f9d15e7f2d876a82452503a1e2
Shawne Laquindanum <shawnebenedict@Outlook.com>
2026-09-25 17:34:52 -0400

    adding place holder

--- backend/internal/supabase/migrations/20260920224806_essays.sql
+++ backend/internal/supabase/migrations/20260920224806_essays.sql
@@ -2,7 +2,8 @@ CREATE TYPE status AS ENUM ('Submitted', 'Draft', 'Review', 'Archived');
 
 CREATE TABLE essays (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    student_id UUID NOT NULL REFERENCES students(id),
+    /* this is a fk table does not exist yet though so its a place holder*/
+    student_id UUID NOT NULL DEFAULT gen_randome_uuid() /*REFERENCES students(id)*/,
     type TEXT NOT NULL,
     college_id BIGINT REFERENCES global_colleges(id),
     link_to_content TEXT NOT NULL,
